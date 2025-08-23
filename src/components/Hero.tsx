893b2933ae5f04c3d1f18c7c4fa4f80b3bed31d0
Romi Gupta <romi35532@gmail.com>
2025-08-24 02:15:53 +0530

    Update Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,10 +28,10 @@ export default function Hero() {
           <code className="block text-left">
             <span className="text-[#49c5b6]">SELECT DISTINCT</span> domain,<br/>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-[#49c5b6]">RANK()</span> <span className="text-[#49c5b6]">OVER</span> (<span className="text-[#49c5b6]">ORDER BY</span> domain) <span className="text-[#49c5b6]">AS</span> Priority_Rank,<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-green-400">'Actively Looking'</span> <span className="text-[#49c5b6]">AS</span> Status<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-green-400">&apos;Actively Looking&apos;</span> <span className="text-[#49c5b6]">AS</span> Status<br/>
             <span className="text-[#49c5b6]">FROM</span> opportunities<br/>
-            <span className="text-[#49c5b6]">WHERE</span> candidate = <span className="text-green-400">'Me'</span><br/>
-            &nbsp;&nbsp;<span className="text-[#49c5b6]">AND</span> domain <span className="text-[#49c5b6]">IN</span> (<span className="text-green-400">'Data Analytics'</span>, <span className="text-green-400">'Data Engineering'</span>);
+            <span className="text-[#49c5b6]">WHERE</span> candidate = <span className="text-green-400">&apos;Me&apos;</span><br/>
+            &nbsp;&nbsp;<span className="text-[#49c5b6]">AND</span> domain <span className="text-[#49c5b6]">IN</span> (<span className="text-green-400">&apos;Data Analytics&apos;</span>, <span className="text-green-400">&apos;Data Engineering&apos;</span>);
           </code>
         </div>
       </div>
